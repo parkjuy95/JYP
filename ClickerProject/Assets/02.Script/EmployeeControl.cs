643961b666baeaf9df3866ca319abaa81b6e307e
parkjuy95 <parkjuy95@naver.com>
2020-02-11 20:25:58 +0900

    2D Clicker Game

---
+++ ClickerProject/Assets/02.Script/EmployeeControl.cs
@@ -0,0 +1,229 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EmployeeControl : MonoBehaviour
+{
+    SpriteRenderer spr;
+
+    public Employee info;
+
+    public float speed;
+
+    public Vector2 prevPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spr = GetComponent<SpriteRenderer>();
+
+        if(string.IsNullOrEmpty(info.name))
+        {
+            SetInfo();
+        }
+        
+        
+
+        StartCoroutine(EanrMoneyAuto());
+        StartCoroutine(Move());
+        StartCoroutine(HpDecreaseAuto());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpriteChange();
+        ShowInfo();
+    }
+
+    IEnumerator EanrMoneyAuto()
+    {
+        while(true)
+        {
+            GameManager.money += 2;
+            ShowTextMoney(2);
+            Debug.Log(GameManager.money);
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+    void ShowTextMoney(int m)
+    {
+        GameObject obj = Instantiate(GameManager.gm.prefabTextMoney, 
+            this.gameObject.transform.Find("Canvas"), false);
+
+        Animator anim = obj.GetComponent<Animator>();
+        anim.SetTrigger("Start");
+
+        Text txt = obj.GetComponent<Text>();
+        txt.text = "+ " + m.ToString("###,###");
+
+        Destroy(obj, 3f);
+    }
+
+    IEnumerator HpDecreaseAuto()
+    {
+        while (true)
+        {
+            info.hp -= 1;
+            
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+    IEnumerator Move()
+    {
+        while(true)
+        {
+            float x = transform.position.x + Random.Range(-2f, 2f);
+            float y = transform.position.y + Random.Range(-2f, 2f);
+
+            Vector2 target = new Vector2(x, y);
+            target = CheckTarget(target);
+
+            prevPosition = transform.position;
+
+            while(Vector2.Distance(transform.position, target) > 0.01f)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, target, speed);
+                yield return null;
+            }
+            
+
+            yield return new WaitForSeconds(1.0f);
+
+        }
+    }
+
+    Vector2 CheckTarget(Vector2 currentTarget)
+    {
+        Vector2 temp = currentTarget;
+
+        if(currentTarget.x < GameManager.gm.limitPoint1.x)
+        {
+            temp = new Vector2(currentTarget.x + 4, temp.y);
+        }
+        else if(currentTarget.x > GameManager.gm.limitPoint2.x)
+        {
+            temp = new Vector2(currentTarget.x - 4, temp.y);
+        }
+
+        if (currentTarget.y > GameManager.gm.limitPoint1.y)
+        {
+            temp = new Vector2(temp.x, currentTarget.y - 4);
+        }
+        else if (currentTarget.y < GameManager.gm.limitPoint2.y)
+        {
+            temp = new Vector2(temp.x, currentTarget.y + 4);
+        }
+
+
+        return temp;
+    }
+    void SpriteChange()
+    {
+        Sprite[] set = null; //0 = 정면, 1 = 후면, 2 = 측면
+
+        if (info.gender == Gender.Female)
+        {
+            set = GameManager.gm.spriteF;
+        }
+        else
+        {
+            set = GameManager.gm.spriteM;
+        }
+        
+        Vector2 abs = (Vector2)transform.position - prevPosition; // 현재 위치값 - 전의 위치값
+        
+
+        if(Mathf.Abs(abs.x) > Mathf.Abs(abs.y)) // x값으로 움직인 절대값 > y값으로 움직인 절대값
+        {
+            spr.sprite = set[2];
+            if(transform.position.x > prevPosition.x) // 오른쪽
+            {
+                spr.flipX = false;
+            }
+            else if(transform.position.x < prevPosition.x) // 왼쪽
+            {
+                spr.flipX = true;
+            }
+        }
+        else // 위 또는 아래
+        {
+            spr.flipX = false;
+            if(transform.position.y > prevPosition.y) // 위
+            {
+                spr.sprite = set[1];
+            }
+            else if(transform.position.y < prevPosition.y) // 아래
+            {
+                spr.sprite = set[0];
+            }
+        }
+    }
+
+    void SetInfo()
+    {
+        info.name = GameManager.familyName + GameManager.name;
+        info.hp = 100;
+
+        info.design = Random.Range(0, 101);
+        info.programming = Random.Range(0, 101);
+        info.sound = Random.Range(0, 101);
+        info.art = Random.Range(0, 101);
+
+        info.salery = Random.Range(100, 1001);
+
+        int r = Random.Range(0, 2);
+        info.gender = (Gender)r;
+    }
+
+    void ShowInfo()
+    {
+        Text txt = gameObject.transform.Find("Canvas/Text_Name").GetComponent<Text>();
+        txt.text = info.name;
+
+        Image img = transform.Find("Canvas/Image/Image_Gauge").GetComponent<Image>();
+        img.fillAmount = info.hp / 100f;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Coffee") == true)
+        {
+            Debug.Log("커피를 마심");
+            info.hp = 100;
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.CompareTag("Employee") == true)
+        {
+            Collider2D col1 = this.gameObject.GetComponent<Collider2D>();
+            Collider2D col2 = collision.collider;
+            Physics2D.IgnoreCollision(col1, col2);
+        }
+    }
+}
+
+public enum Gender
+{
+    Female = 0,
+    Male = 1
+}
+
+[System.Serializable]
+public class Employee
+{
+    public string name; //이름
+    public Gender gender; // 성별
+    public float design; //기획
+    public float programming; //프로그래밍
+    public float art; //그림
+    public float sound; //사운드
+
+    public float hp; //체력
+
+    public long salery; //월급
+}
---
+++ ClickerProject/Assets/02.Script/GameManager.cs
@@ -0,0 +1,322 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gm;
+    public static long money;
+
+    public GameObject prefabCoffee;
+    public GameObject prefabEmployee;
+    public GameObject prefabTextMoney;
+
+    public Sprite[] spriteF;
+    public Sprite[] spriteM;
+
+    public Vector2 limitPoint1;
+    public Vector2 limitPoint2;
+
+    public Text textMoney;
+
+    public static string familyName
+    {
+        get
+        {
+            string[] names = new string[10];
+
+            names[0] = "김";
+            names[1] = "이";
+            names[2] = "박";
+            names[3] = "최";
+            names[4] = "정";
+            names[5] = "강";
+            names[6] = "조";
+            names[7] = "윤";
+            names[8] = "장";
+            names[9] = "임";
+
+            int r = Random.Range(0, names.Length);
+            string s = names[r];
+
+            return s;
+        }
+    }
+    public static string name
+    {
+        get
+        {
+            string[] names = new string[10];
+
+            names[0] = "기역";
+            names[1] = "니은";
+            names[2] = "디귿";
+            names[3] = "리을";
+            names[4] = "미음";
+            names[5] = "비읍";
+            names[6] = "시옷";
+            names[7] = "이응";
+            names[8] = "지읒";
+            names[9] = "치읓";
+
+            int r = Random.Range(0, names.Length);
+            string s = names[r];
+
+            return s;
+        }
+    }
+
+    public Sprite[] spriteButtonState;
+    public Image imgButton;
+    public bool isWhip;
+
+    public GameObject panelInfo;
+
+    public List<Employee> emps;
+
+    private string savePath;
+
+    public AudioClip clip;
+
+    private void Awake()
+    {
+        gm = this;
+        savePath = Application.persistentDataPath + "/save.sav";
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        emps = new List<Employee>();
+        money = 100000;
+        if(System.IO.File.Exists(savePath))
+        {
+            Load();
+            foreach(var a in emps)
+            {
+                InitioalizeEmployee(a);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //EarnMoney();
+        if(EventSystem.current.IsPointerOverGameObject() == false)
+        {
+            if (isWhip == true)
+                EarnMoney();
+            else
+                CreateCoffee();
+        }
+
+        if(GameObject.Find("Panel_Option") != null)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+            GetComponent<AudioSource>().PlayOneShot(clip);
+
+        ShowMoneyText();
+        ChangeButtonSprite();
+
+    }
+
+    public void EarnMoney()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            money += 1;
+            Debug.Log(money);
+        }
+    }
+
+    void CreateCoffee()
+    {
+        if(Input.GetMouseButtonDown(0)) // 마우스 왼쪽 버튼을 클릭하면
+        {
+            Vector2 mousePosition = 
+                Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            //게임 오브젝트 생성
+            Instantiate(prefabCoffee, mousePosition, Quaternion.identity);
+        }
+    }
+
+    void ShowMoneyText()
+    {
+        textMoney.text = money.ToString("###,###");
+    }
+
+    public void ChangeButtonState()
+    {
+        if (isWhip == true)
+            isWhip = false;
+        else
+            isWhip = true;
+    }
+
+    void ChangeButtonSprite()
+    {
+        if (isWhip == true)
+        {
+            imgButton.sprite = spriteButtonState[0]; // 채찍
+            imgButton.rectTransform.sizeDelta = new Vector2(180, 180);
+        }
+
+        else
+        { 
+            imgButton.sprite = spriteButtonState[1]; //커피
+            imgButton.rectTransform.sizeDelta = new Vector2(110, 180);
+        }
+    }
+
+    public void PanelHireOpen()
+    {
+        panelInfo.SetActive(true);
+
+
+        Employee e = RandomCreateEmployee();
+
+        var textName = panelInfo.transform.Find("Text_Name").GetComponent<Text>();
+        var imageCharactoer = panelInfo.transform.Find("Image_Character").GetComponent<Image>();
+        var textProgramming = panelInfo.transform.Find("Text_Programming").GetComponent<Text>();
+        var textDesign = panelInfo.transform.Find("Text_Design").GetComponent<Text>();
+        var textSound = panelInfo.transform.Find("Text_Sound").GetComponent<Text>();
+        var textArt = panelInfo.transform.Find("Text_Art").GetComponent<Text>();
+        var textSalery = panelInfo.transform.Find("Button_Hire/Image_Price/Text").GetComponent<Text>();
+        var buttonHire = panelInfo.transform.Find("Button_Hire").GetComponent<Button>();
+
+        textName.text = e.name;
+        if(e.gender == Gender.Female)
+        {
+            imageCharactoer.sprite = spriteF[0];
+        }
+        else
+        {
+            imageCharactoer.sprite = spriteM[0];
+        }
+
+        textProgramming.text = e.programming.ToString();
+        textDesign.text = e.design.ToString();
+        textSound.text = e.sound.ToString();
+        textArt.text = e.art.ToString();
+        textSalery.text = e.salery.ToString();
+
+        buttonHire.onClick.RemoveAllListeners();
+        buttonHire.onClick.AddListener(delegate
+        {
+            Hire((int)e.salery, e);
+        });
+    }
+
+    Employee RandomCreateEmployee()
+    {
+        Employee info = new Employee();
+
+        info.name = familyName + name;
+        info.hp = 100;
+
+        info.design = Random.Range(0, 101);
+        info.programming = Random.Range(0, 101);
+        info.sound = Random.Range(0, 101);
+        info.art = Random.Range(0, 101);
+
+        info.salery = Random.Range(100, 1001);
+
+        int r = Random.Range(0, 2);
+        info.gender = (Gender)r;
+
+        return info;
+    }
+
+    void CreateEmployee(Employee e)
+    {
+        GameObject obj = Instantiate(prefabEmployee, Vector3.zero, Quaternion.identity);
+        obj.GetComponent<EmployeeControl>().info = e;
+    }
+
+    void InitioalizeEmployee(Employee e)
+    {
+        GameObject obj = Instantiate(prefabEmployee, Vector3.zero, Quaternion.identity);
+        obj.GetComponent<EmployeeControl>().info = e;
+    }
+
+    public void Hire(int price, Employee e)
+    {
+        if(money >= price)
+        {
+            CreateEmployee(e);
+            money -= price;
+            emps.Add(e);
+        }
+    }
+
+    public void Save()
+    {
+        //PlayerPrefs.SetInt("MONEY", (int)money);
+        //PlayerPrefs.SetString("MONEY", money.ToString());
+        SaveData sd = new SaveData();
+        sd.money = money;
+        sd.empList = emps;
+        XmlManager.XmlSave<SaveData>(sd, savePath);
+    }
+
+    public void Load()
+    {
+        //money = PlayerPrefs.GetInt("MONEY", 1000);
+        //money = long.Parse(PlayerPrefs.GetString("MONEY"));
+        SaveData sd = XmlManager.XmlLoad<SaveData>(savePath);
+        money = sd.money;
+        emps = sd.empList;
+    }
+
+    private void OnApplicationQuit()
+    {
+        Save();
+        Application.Quit();
+    }
+    private void OnDrawGizmos()
+    {
+        Vector2 limitPoint3 = new Vector2(limitPoint2.x, limitPoint1.y);
+        Vector2 limitPoint4 = new Vector2(limitPoint1.x, limitPoint2.y);
+
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawLine(limitPoint1, limitPoint3);
+        Gizmos.DrawLine(limitPoint3, limitPoint2);
+        Gizmos.DrawLine(limitPoint1, limitPoint4);
+        Gizmos.DrawLine(limitPoint4, limitPoint2);
+
+    }
+
+    public void VolumeChange(Slider sl)
+    {
+        GetComponent<AudioSource>().volume = sl.value;
+    }
+
+    public void SaveDelete()
+    {
+        if(System.IO.File.Exists(savePath))
+        {
+            System.IO.File.Delete(savePath);
+        }
+    }
+}
+
+
+public class SaveData
+{
+    public long money;
+
+    public List<Employee> empList;
+}
